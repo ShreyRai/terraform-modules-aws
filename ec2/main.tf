8754a2d9e07933902a7474affd96573a29434094
Shreyash Raichand <shreyrai20@gmail.com>
2026-08-15 18:12:57 +0530

    ec2 modules change 2

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "ec201" {
     ami = var.ami
     instance_type = var.instance_type
+    associate_public_ip_address = true
     tags = {
         Name = var.instance_name
     }
--- ec2/output.tf
+++ ec2/output.tf
@@ -5,7 +5,3 @@ output "public_ip" {
 output "instance_id" {
     value = aws_instance.ec201.id
 }
-
-output "private_ip" {
-  value = aws_instance.ec201.private_ip
-}
